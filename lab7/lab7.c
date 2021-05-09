9b210a6a2cc0fbe1b712362b694609bf00ba5dd8
Daniela <mor19213@uvg.edu.gt>
2021-05-09 17:42:02 -0600

    interrupcion uart

--- lab7/lab7.c
+++ lab7/lab7.c
@@ -10,7 +10,7 @@
 #include "driverlib/timer.h"
 #include "driverlib/uart.h"
 
-
+ void uartIntHandler(void);
 uint32_t ui32Period = 0;
 uint32_t recibido = 0;
 uint8_t rojo = 0;
@@ -50,12 +50,16 @@ int main(void)
            GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1);  // rojo
            GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_3);  // verde
 
+           IntMasterEnable();
+           IntEnable(INT_UART0);
+
+
            InitUART();
 
        SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER0);
        // CONFIG del timer0 como temporizador periodico
        TimerConfigure(TIMER0_BASE, TIMER_CFG_PERIODIC);
-       ui32Period = (SysCtlClockGet())*2;   // periodo para el temporizador (1segundo)
+       ui32Period = (SysCtlClockGet())*2;   // periodo para el temporizador (2 segundos)
        TimerLoadSet(TIMER0_BASE, TIMER_A, ui32Period -  1); // establece el periodo para el timer
        IntEnable(INT_TIMER0A);
        TimerIntEnable(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
@@ -63,12 +67,15 @@ int main(void)
        TimerEnable(TIMER0_BASE, TIMER_A);
 
        while(1){
-           uart_rx();
            }
 
 }
 
 
+void UARTIntHandler(void){
+    uart_rx();
+    //UARTCharPut(UART0_BASE, 'a');
+}
 
 void Timer0IntHandler(void){
     TimerIntClear(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
@@ -109,18 +116,15 @@ void InitUART(void){
     GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
 
     /* Sets the configuration of a UART. */
-    UARTConfigSetExpClk(UART0_BASE, SysCtlClockGet(), 115200,
-    (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
+    UARTConfigSetExpClk(UART0_BASE, SysCtlClockGet(), 115200, (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
 
     UARTIntEnable(UART0_BASE, UART_INT_RX | UART_INT_TX);
 
-    UARTCharPut(UART0_BASE,'H');
+    IntEnable(INT_UART0);
+    UARTIntEnable(UART0_BASE, UART_INT_RX | UART_INT_RT);
 
 }
 
-void UARTIntHandler(void){
-
-}
 
 void uart_rx(void){
     recibido = UARTCharGet(UART0_BASE);
